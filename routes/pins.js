075278fb74dcf9c55e622e7a73211936b4bc2b24
Biswajit <biswajitghosh150803@gmail.com>
2023-06-18 21:27:00 +0530

    ..

--- routes/pins.js
+++ routes/pins.js
@@ -3,20 +3,39 @@ const Pin = require("../models/Pin");
 // const {formDel} = require("../middleware/middleware");
 //create a pin
 router.post("/", async (req, res) => {
-  const orders = await Pin.deleteMany({
-    username: req.body.username,
-  });
-  console.log(orders);
+          const orders = await Pin.updateOne(  
+            {
+                "username": req.body.username                  
+            },{
+              lat: req.body.lat,
+              long: req.body.long,
+              title: req.body.title,
+              desc: req.body.desc,
+            },{
+              upsert: true,
+            }
+        );
+        console.log(orders);
+        
 
-  const newPin = new Pin(req.body);
-  try {
-    const savedPin = await newPin.save();
-    res.status(200).json(savedPin);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // const newPin = new Pin(req.body);
+  // try {
+  //   const savedPin = await newPin.save();
+  //   res.status(200).json(savedPin);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
+
+router.post("/del", async (req, res) => {
+  const orders = await Pin.deleteOne(  
+    {
+        "username": req.body.username                  
+    }
+);
+console.log(orders);
+  });
 //get all pins
 router.get("/", async (req, res) => {
   try {
@@ -28,19 +47,31 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/del", async (req, res) => {
-  const orders = await Pin.deleteMany({
-    username: req.body.username,
-  });
-  console.log(orders);
+router.route("/updatepins").post((req,res)=>{
+  
+  Pin.updateMany({"username":req.body.username},{
+    lat: req.body.lat,
+    long: req.body.long,
+  }, function (err, raw) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    } else {
+      console.log(raw);
+      res.status(200);
+      res.send();
+    }
 });
+});
+
 
 router.get("/category", async (req, res) => {
   try {
-    const cat = req.query.Category;
-    const pins = await Pin.find({ title: cat });
+    console.log(req.query);
+    const cat=req.query.Category;
+    const pins = await Pin.find({title:cat});
     res.status(200).json(pins);
-    console.log();
+    console.log(req.query);
   } catch (err) {
     res.status(500).json(err);
   }
